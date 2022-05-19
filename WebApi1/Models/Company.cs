6f02605252fbb91e4900038a3ebf4581da7b1f9d
Davide Mauri <damauri@microsoft.com>
2022-05-19 21:37:14 +0000

    remove null warning

--- WebApi1/Models/Company.cs
+++ WebApi1/Models/Company.cs
@@ -6,7 +6,7 @@ namespace WebApi1.Models
 	public class Company
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
+		public string? Name { get; set; }
 		public string? CatchPhrase { get; set; }
 		public int FoundationYear { get; set; }
 		public bool IsFullRemote { get; set; }
